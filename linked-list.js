dd7c2deec982086e56b023410a9148f001c538a0
Larry Kagan <larry@superiocity.com>
2020-05-31 11:25:12 -0400

    Append, prepend, insert doubly linked list in JS

--- linked-list.js
+++ linked-list.js
@@ -1,4 +1,4 @@
-class LinkedList 
+class LinkedList
 {
     /**
      * Setup the head and tail nodes of the list
@@ -19,8 +19,9 @@ class LinkedList
      */
     append(value) 
     {
-        this.tail.next = new Node(value);
-        this.tail = this.tail.next;
+        const newNode = new Node(value, this.tail);
+        this.tail.next = newNode;
+        this.tail = newNode;
         return ++this.length;
     }
 
@@ -32,7 +33,9 @@ class LinkedList
      */
     prepend(value) 
     {
-        this.head = new Node(value, this.head);
+        const newNode = new Node(value, null, this.head);
+        this.head.previous = newNode;
+        this.head = newNode;
         return ++this.length;
     }
 
@@ -49,8 +52,12 @@ class LinkedList
             return this.prepend(value);
         }
 
-        const existingNode = this.nodeAtIndex(index - 1);
-        existingNode.next = new Node(value, existingNode.next);
+        const existingNode = this.nodeAtIndex(index);
+        const previousNode = existingNode.previous;
+        const newNode      = new Node(value, previousNode, existingNode);
+
+        existingNode.previous = previousNode.next = newNode;
+
         return ++this.length;
     }
 
@@ -94,14 +101,28 @@ class LinkedList
             current = current.next;
         }
     }
+
+    print()
+    {
+        let node   = this.head;
+        let values = [];
+
+        do {
+            values.push(node.value);
+            node = node.next;
+        } while(node);
+
+        console.log('[' + values.join(', ') + ']');
+    }
 }
 
 class Node
 {
-    constructor(value, next = null)
+    constructor(value, prev = null, next = null)
     {
-        this.value = value;
-        this.next = next;
+        this.value    = value;
+        this.next     = next;
+        this.previous = prev;
     }
 }
 
@@ -110,4 +131,4 @@ list.append(5);
 list.prepend(12);
 list.insert(1, 4);
 list.delete(1);
-console.log(JSON.stringify(list.head));
+list.print();
